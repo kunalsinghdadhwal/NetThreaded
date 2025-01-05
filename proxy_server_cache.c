3a9d2e7a8787664b5afe6ecd03fc478f9d33ca09
Kunal Singh Dadhwal <155142500+Kunal-Singh-Dadhwal@users.noreply.github.com>
2025-01-05 16:21:04 +0530

    Handle Request Function

--- proxy_server_cache.c
+++ proxy_server_cache.c
@@ -41,6 +41,105 @@ pthread_mutex_t lock;
 cache_element *head = NULL;
 int cache_size;
 
+int connnectRemoteServer(char *host_addr, int port_num)
+{
+    int remoteSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (remoteSocket < 0)
+    {
+        printf("Error in creating your Socket\n");
+        return -1;
+    }
+
+    struct hostent *host = gethostbyname(host_addr);
+    if (host == NULL)
+    {
+        fprintf(stderr, "No such hosts exist\n");
+        return -1;
+    }
+    struct sockaddr_in server_addr;
+    bzero((char *)&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port_num);
+
+    bcopy((char *)host->h_addr_list[0], (char *)&server_addr.sin_addr.s_addr, host->h_length);
+
+    if (connect(remoteSocket, (struct sockaddr *)&server_addr, (size_t)sizeof(server_addr)) < 0)
+    {
+        fprintf(stderr, "Error in Connecting\n");
+        return -1;
+    }
+    return remoteSocket;
+}
+
+int handle_request(int clientSocketId, struct ParsedRequest *request, char *tempReq)
+{
+    char *buffer = (char *)calloc(MAX_BYTES, sizeof(char));
+    strcpy(buffer, "GET ");
+    strcpy(buffer, request->path);
+    strcat(buffer, " ");
+    strcat(buffer, request->version);
+    strcat(buffer, "\r\n");
+    size_t len = strlen(buffer);
+
+    if (ParsedHeader_set(request, "Connection", "close") < 0)
+    {
+        printf("Set Connection header key not working\n");
+    }
+
+    if (ParsedHeader_get(request, "Host") == NULL)
+    {
+        if (ParsedHeader_set(request, "Host", request->host) < 0)
+        {
+            printf("Set Host header key not working\n");
+        }
+    }
+    if (ParsedRequest_unparse_headers(request, buffer + len, (size_t)MAX_BYTES - len) < 0)
+    {
+        printf("Unparse headers failed\n");
+    }
+
+    int server_port = 80;
+    if (request->port != NULL)
+    {
+        server_port = atoi(request->port);
+    }
+    int remoteSocketId = connnectRemoteServer(request->host, server_port);
+    if (remoteSocketId < 0)
+    {
+        return -1;
+    }
+    int bytes_send = send(remoteSocketId, buffer, strlen(buffer), 0);
+    bytes_send = recv(remoteSocketId, buffer, MAX_BYTES - 1, 0);
+    char *temp_buffer = (char *)calloc(MAX_BYTES, sizeof(char));
+    int temp_buffer_size = MAX_BYTES;
+    int temp_buffer_index = 0;
+
+    while (bytes_send > 0)
+    {
+        bytes_send = send(clientSocketId, buffer, bytes_send, 0);
+        for (int i = 0; i < bytes_send / sizeof(char); i++)
+        {
+            temp_buffer[temp_buffer_index] = buffer[i];
+            temp_buffer_index++;
+        }
+        temp_buffer_size += MAX_BYTES;
+        temp_buffer = (char *)realloc(temp_buffer, temp_buffer_size);
+        if (bytes_send < 0)
+        {
+            perror("Error in sending data to client\n");
+            break;
+        }
+        bzero(buffer, MAX_BYTES);
+        bytes_send = recv(remoteSocketId, buffer, MAX_BYTES - 1, 0);
+    }
+    temp_buffer[temp_buffer_index] = '\0';
+    free(buffer);
+    add_cache_element(temp_buffer, strlen(temp_buffer), tempReq);
+    free(temp_buffer);
+    close(remoteSocketId);
+    return 0;
+}
+
 void *thread_fn(void *socketNew)
 {
     sem_wait(&semaphore);
@@ -173,7 +272,7 @@ int main(int argc, char const *argv[])
     if (bind(proxy_socketId, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
     {
         perror("Port is not available");
-        exit(1)
+        exit(1);
     }
     printf("Binding on Port %d\n", PORT);
     int listen_status = listen(proxy_socketId, MAX_CLIENTS);
